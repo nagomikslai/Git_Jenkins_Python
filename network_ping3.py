da17aec6d2af5bf75681c1422d039418e201044d
kslai <43594906+nagomikslai@users.noreply.github.com>
2024-03-15 15:06:16 +0800

    add network ping 3 program

---
+++ network_ping3.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+def ping_ip(ip_address):
+    """
+    Ping IP address and return tuple:
+    On success:
+        * True
+        * command output (stdout)
+    On failure:
+        * False
+        * error output (stderr)
+    """
+    reply = subprocess.run(['ping', '-l', '3', '-n', '3', ip_address],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           encoding='utf-8')
+    if reply.returncode == 0:
+        return True, reply.stdout
+    else:
+        return False, reply.stderr
+
+print(ping_ip('www.google.com'))
+print(ping_ip('a'))
